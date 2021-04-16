5296f2db1e95fa70499a1ace17a452e97bb1edb7
Masashi Mori <a17.gt37@g.chuo-u.ac.jp>
2021-04-16 21:16:43 +0900

    余計な記述を削除

--- shell_scripts/template/run.sh
+++ shell_scripts/template/run.sh
@@ -1,6 +1,5 @@
-docker run --gpus all --privileged --name container_name --net=host \ # You can change container name using set-container-name 
+docker run --gpus all --privileged --name container_name --net=host \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    # -v /dev:/dev \ # if you want to use USB devices etc... in container, you have to delete comment
     -itd nvidia/cudagl-10.1-devel-ubuntu18.04:ros-melodic /bin/bash
 
