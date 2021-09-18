c2d62e324cad911bff5285de7e32af8477834594
Masashi Mori <a17.gt37@g.chuo-u.ac.jp>
2021-09-19 01:49:30 +0900

    Fix volume path

--- shell_scripts/template/run.sh
+++ shell_scripts/template/run.sh
@@ -1,6 +1,6 @@
 docker run --gpus all --privileged --name container_name --net=host \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v `pwd`/src:/root/catkin_ws/src \
+    -v `pwd`/src:/root/ros2_ws/src \
     -itd nvidia/cudagl-11.4.1-devel-ubuntu20.04:ros2-foxy /bin/bash
 
